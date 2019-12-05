4997131bcbd5dc1ec6d9c013a7ecfae4ffa39e66
lushijie <lushijie03@meituan.com>
2019-12-05 10:38:42 +0800

    demo3x

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/life/demo11';
+import App from './components/life/demo12';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
